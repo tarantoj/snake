4f7c28da5b647bef813e0a53289cfd39d99b84ed
James Taranto <taranto.james@gmail.com>
2018-05-25 12:42:24 +1000

    Add food

--- main.c
+++ main.c
@@ -19,6 +19,50 @@ typedef struct food {
 } food_t;
 
 int row, col;
+static void spawn_food(snake_t *snake, food_t *food) {
+	int x, y;
+	x = rand() % col;
+	y = rand() % row;
+	bool flag = false;
+
+	snake_t *temp;
+	temp = snake;
+
+	while(temp) {
+		if (x == temp->x && y == temp->y) {
+			flag = true;
+			break;
+		}
+		temp = temp->next;
+	}
+
+	if (flag == true) {
+		spawn_food(snake, food);
+	} else {
+		food->x = x;
+		food->y = y;;
+	}
+}
+
+
+static void add_part(snake_t * snake)
+{
+	snake_t *temp, *prev;
+	temp = snake;
+
+	while (temp->next) {
+		temp = temp->next;
+	}
+
+	temp->next = malloc(sizeof(snake_t));
+	prev = temp;
+	temp = temp->next;
+	temp->x = prev->x - 1;
+	temp->y = prev->y;
+	temp->dir = prev->dir;
+	temp->next = NULL;
+	temp->prev = prev;
+}
 
 static void init(void)
 {
@@ -46,7 +90,7 @@ static void init_snake(snake_t * snake)
 	snake->next->prev = snake;
 }
 
-static void print_snake(snake_t * snake, FILE * log_file)
+static void print_snake(snake_t * snake)
 {
 	clear();
 	snake_t *temp;
@@ -81,7 +125,7 @@ static void print_snake(snake_t * snake, FILE * log_file)
 
 }
 
-static void move_snake(snake_t * snake, FILE * log_file)
+static void move_snake(snake_t * snake, food_t *food)
 {
 	snake_t *temp;
 	temp = snake;
@@ -153,44 +197,39 @@ static void move_snake(snake_t * snake, FILE * log_file)
 		break;
 	}
 
-}
-
-static void add_part(snake_t * snake)
-{
-	snake_t *temp, *prev;
-	temp = snake;
-
-	while (temp->next) {
-		temp = temp->next;
+	if (snake->x == food->x && snake->y == food->y) {
+		add_part(snake);
+		spawn_food(snake, food);
 	}
 
-	temp->next = malloc(sizeof(snake_t));
-	prev = temp;
-	temp = temp->next;
-	temp->x = prev->x - 1;
-	temp->y = prev->y;
-	temp->dir = prev->dir;
-	temp->next = NULL;
-	temp->prev = prev;
 }
+
+
+static void print_food(food_t *food)
+{
+	mvaddch(food->y, food->x, '*');
+}
+
+
 
 int main(void)
 {
 	init();
 
-	FILE *log_file;
-
-	log_file = fopen("log.txt", "w");
 
 	snake_t *snake;
 	snake = malloc(sizeof(snake_t));
+	food_t *food;
+	food = malloc(sizeof(food_t));
 	init_snake(snake);
 	add_part(snake);
 	add_part(snake);
+	spawn_food(snake, food);
 
 	while (1) {
-		print_snake(snake, log_file);
-		move_snake(snake, log_file);
+		print_snake(snake);
+		print_food(food);
+		move_snake(snake, food);
 		if (getch() == 'q')
 			break;
 		sleep(1);
@@ -198,7 +237,6 @@ int main(void)
 	}
 
 	endwin();
-	fclose(log_file);
 
 	return 0;
 }
